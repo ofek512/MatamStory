eee4d9cb440a9844a1940a63243c3738e29d7038
ofek5 <ofekikosh123@gmail.com>
2024-08-05 20:36:09 +0300

    add declaration and impl of Event, Monster and Balrog. Add comments for future checking

---
+++ Events/Event.cpp
@@ -0,0 +1,36 @@
+#include "Event.h"
+
+// Initialize static members
+unsigned int Balrog::combatPower = 15;
+bool Balrog::initialized = false;
+
+
+// Monster Constructor
+Monster::Monster(string xName, unsigned int xPower, unsigned int xLoot, unsigned int xDamage)
+    : name(xName), power(xPower), loot(xLoot), damage(xDamage) {}
+
+// Monster Destructor
+Monster::~Monster() = default;
+
+// Monster runEvent Implementation
+void Monster::runEvent(list<Player> players) {
+    // Base implementation for Monster events (could be empty or provide some basic functionality)
+}
+
+// Balrog Constructor
+Balrog::Balrog(string xName, unsigned int xPower, unsigned int xLoot, unsigned int xDamage)
+    : Monster(xName, xPower, xLoot, xDamage) {
+    if (!initialized) {
+        combatPower = xPower; // Initialize combat power only once
+        initialized = true;
+    }
+}
+
+// Balrog Destructor
+Balrog::~Balrog() = default;
+
+// Balrog runEvent Implementation
+void Balrog::runEvent(list<Player> players) {
+    combatPower += 2; // Increase combat power by 2 for each event
+    // Implement the specific event logic for Balrog here
+}
--- Events/Event.h
+++ Events/Event.h
@@ -1,14 +1,40 @@
-
 #pragma once
-
+#include <list>
+#include <string>
+#include "Player.h"
 #include "../Players/Player.h"
+
+using std::list;
+using std::string;
 
 class Event {
 public:
-    /**
-     * Gets the description of the event
-     *
-     * @return - the description of the event
-    */
-    string getDescription() const;
+    virtual ~Event() = 0; //check if this should be 0
+    virtual void runEvent(list<Player> players) = 0;
+    string getDescription() const; // maybe make virtual? :p
+};
+
+
+class Monster : public Event {
+protected:
+    string name;
+    unsigned int power;
+    unsigned int loot;
+    unsigned int damage;
+public:
+    Monster(string xName, unsigned int xPower, unsigned int xLoot, unsigned int xDamage);
+    ~Monster() override;
+    void runEvent(list<Player> players) override;
+};
+
+
+class Balrog : public Monster {
+public:
+    Balrog(string xName, unsigned int xPower, unsigned int xLoot, unsigned int xDamage);
+    ~Balrog() override;
+    void runEvent(list<Player> players) override;
+
+private:
+    static unsigned int combatPower;
+    static bool initialized;
 };
