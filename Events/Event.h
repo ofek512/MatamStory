970a4539ba62944cf11b0f7b1ac54a98542ba8e7
pavelrussko <pavelrussko@gmail.com>
2024-08-06 15:44:23 +0300

    Add Encounter

--- Events/Event.h
+++ Events/Event.h
@@ -10,33 +10,8 @@ using std::string;
 
 class Event {
 public:
-    virtual ~Event() = 0; //check if this should be 0
-    virtual void runEvent(list<Player> players) = 0;
-    string getDescription() const; // maybe make virtual? :p
+    virtual ~Event() = default; //check if this should be 0
+    virtual void runEvent(Player& player) = 0;
+    virtual string getDescription() const; // maybe make virtual? :p
 };
 
-
-class Monster : public Event {
-protected:
-    string name;
-    unsigned int power;
-    unsigned int loot;
-    unsigned int damage;
-public:
-    Monster(string xName, unsigned int xPower, unsigned int xLoot,
-            unsigned int xDamage);
-    ~Monster() override;
-    void runEvent(list<Player> players) override;
-};
-
-
-class Balrog : public Monster {
-public:
-    Balrog(string xName);
-    ~Balrog() override;
-    void runEvent(list<Player> players) override;
-
-private:
-    static unsigned int combatPower;
-    static bool initialized;
-};
---
+++ Events/Monster.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Encounter.h"
+#include "Event.h"
+
+
+class Monster : public Encounter {
+protected:
+    string name;
+    unsigned int power;
+    unsigned int loot;
+    unsigned int damage;
+public:
+    Monster(string xName = "", unsigned int xPower = 0, unsigned int xLoot = 0,
+            unsigned int xDamage = 0);
+    ~Monster() override;
+    void runEvent(Player&) override;
+    int getPower();
+    int getLoot();
+    int getDamage();
+};
+
