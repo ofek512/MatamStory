87250227e4f3d123f967ab48f8646d0f70743bea
Ofek <ofek5202@gmail.com>
2024-08-06 16:49:09 +0300

    Add solareclipse decl and impl

--- Events/Event.h
+++ Events/Event.h
@@ -1,17 +1,13 @@
-#include <list>
-#include <string>
-#include "Player.h"
-#include "../Players/Player.h"
-
 #pragma once
 
-using std::list;
+#include "Player.h"
+
 using std::string;
 
 class Event {
 public:
     virtual ~Event() = default; //check if this should be 0
-    virtual void runEvent(Player &player) = 0;
+    virtual void runEvent(Player &) = 0;
     virtual string getDescription() const; // maybe make virtual? :p
 };
 
--- Events/SolarEclipse.cpp
+++ Events/SolarEclipse.cpp
@@ -1,2 +1,5 @@
 #include "SolarEclipse.h"
 
+void SolarEclipse::runEvent(Player &player) {
+    player.doEclipse();
+}
